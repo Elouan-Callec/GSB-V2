13744e337861bc05f15a810995befbd19d7fa13d
Erwan FAVE <efave@estran-brest.education>
2022-03-25 10:58:45 +0100

    Changement de page entre InterfaceDelegueregional et AfficheDelegueregional activé

--- GSB-V2/InterfaceDelegueregional.vb
+++ GSB-V2/InterfaceDelegueregional.vb
@@ -2,18 +2,22 @@
     Dim myConnection As New Odbc.OdbcConnection
     Dim myCommand As New Odbc.OdbcCommand
     Dim myReader As Odbc.OdbcDataReader
+    Dim testprenom As String = "JONES"
+    Dim testnom As String = "JONESS"
 
 
     Private Sub InterfaceDelegueregional_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim affichePrenom As String = "SELECT ename FROM emp WHERE login ='JONES';"
 
-        myCommand.Connection = FormulaireConnexion.myConnection
-        myCommand.CommandText = affichePrenom
-        myReader = myCommand.ExecuteReader()
+        Labelprenom.Text = testprenom
+        Labelnom.Text = testnom
 
-        'Labelprenom.Text = 
-        myReader.Read()
+    End Sub
 
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'AfficheDelegueregional.Show()
+        'Me.Close()
+        Me.Hide()
+        AfficheDelegueregional.Show()
     End Sub
 End Class
