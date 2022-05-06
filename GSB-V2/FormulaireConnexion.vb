11ff4b393b76888a0d3ca0c2b0719a7d81451d06
Erwan FAVE <efave@estran-brest.education>
2022-05-06 09:21:11 +0200

    Connexion en fonction du role

--- GSB-V2/FormulaireConnexion.vb
+++ GSB-V2/FormulaireConnexion.vb
@@ -1,5 +1,7 @@
 ﻿Public Class FormulaireConnexion
 
+
+
     Dim myConnection As New Odbc.OdbcConnection
     Dim myCommand As New Odbc.OdbcCommand
     Dim myReader As Odbc.OdbcDataReader
@@ -7,10 +9,16 @@
     Dim myBuilder As Odbc.OdbcCommandBuilder
     Dim connString As String
 
+
+
     Private Sub FormulaireConnexion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        connString = "DSN=CNXORA06;Uid=SCOTT;Pwd=TIGER;"
+        connString = "DSN=CNXORA06;Uid=SYSTEM;Pwd=Iroise29;"
+
+
 
         myConnection.ConnectionString = connString
+
+
 
         Try
             myConnection.Open()
@@ -20,23 +28,52 @@
         End Try
     End Sub
 
+
     Private Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
         Dim login As String = TextBoxlogin.Text
         Dim password As String = TextBoxmotdepasse.Text
 
-        Dim query As String = "SELECT count(*) FROM emp WHERE login = '" + login + "' AND password = '" + password + "';"
+
+
+        Dim query As String = "SELECT count(*) FROM VISITEUR WHERE login = '" + login + "' AND password = '" + password + "';"
         myCommand.Connection = myConnection
         myCommand.CommandText = query
         myReader = myCommand.ExecuteReader
 
-        myReader.Read()
 
+
+        myReader.Read()
         If (myReader.GetString(0) = 1) Then
             MessageBox.Show("Connexion réussie")
-            RedigerCompteRendu.Show()
-            Me.Close()
+            myReader.Close()
+
+            Dim ID_role = "SELECT ROLE FROM VISITEUR WHERE login ='" + login + "';"
+            myCommand.CommandText = ID_role
+            myReader = myCommand.ExecuteReader
+
+            myReader.Read()
+
+            If (myReader.GetString(0) = 1) Then
+
+            End If
+
+            If (myReader.GetString(0) = 2) Then
+                InterfaceDelegueregional.Show()
+                Me.Close()
+            End If
+
+            If (myReader.GetString(0) = 3) Then
+
+            End If
+
         Else
             MessageBox.Show("Connexion échouée")
         End If
     End Sub
 End Class
+
+
+
+
+
+
