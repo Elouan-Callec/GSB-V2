de651758c8f8c02c217f40b0346d39cdf810e403
Erwan FAVE <efave@estran-brest.education>
2022-05-10 11:46:51 +0200

    Modification lien bouton Rediger un compte rendu

--- GSB-V2/ProfilDelegueregional.vb
+++ GSB-V2/ProfilDelegueregional.vb
@@ -56,19 +56,25 @@ Public Class ProfilDelegueregional
 
     End Sub
 
+    'Selectionner une activite
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonSelectionActivite.Click
         SelectionActivite.Show()
         Me.Close()
     End Sub
 
-    Private Sub Buttonredigercompterendu_Click(sender As Object, e As EventArgs)
+    'Selectionner un compte rendu 
+    Private Sub ButtonVoirCompteRendu_Click(sender As Object, e As EventArgs) Handles ButtonVoirCompteRendu.Click
+        ChoixRedacteurCompteRendu.Show()
+        Me.Close()
+    End Sub
+
+
+    'Rediger un compte rendu
+    Private Sub ButtonRedigerCompteRendu_Click(sender As Object, e As EventArgs) Handles ButtonRedigerCompteRendu.Click
         AffichageActivite.Show()
         Me.Close()
 
     End Sub
 
-    Private Sub ButtonVoirCompteRendu_Click(sender As Object, e As EventArgs) Handles ButtonVoirCompteRendu.Click
-        ChoixRedacteurCompteRendu.Show()
-        Me.Close()
-    End Sub
+
 End Class
