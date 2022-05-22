b7e3eb17b6db64e4a6fe0a8a8c5b411fd62d1e6e
Favé Erwan <erwanfave28@gmail.com>
2022-05-23 00:10:01 +0200

    Modification AffichageCompteRendu

--- GSB-V2/AffichageCompteRendu.vb
+++ GSB-V2/AffichageCompteRendu.vb
@@ -36,7 +36,42 @@
             LabelMedecin.Text = Medecin
 
 
+        ElseIf (SelectionCompteRendu.ValeurBouton1 = 1) Then
+
+
+            Dim login As String = BDD.afficheLogin()
+            Dim idVisiteur = BDD.afficheIdVisiteur(login)
+            Dim dte = SelectionCompteRendu.ComboBoxCompteRendu.Text.Substring(16, 10)
+            dte = dte.Remove(7, 2)
+            Dim datefinal As DateTime = dte
+            Dim ID = BDD.RecuperationIDCompteRendu(idVisiteur, datefinal)
+            Dim Bilan = BDD.AfficheBilan(ID)
+            Dim QTE_Medoc = BDD.AfficheQTE_Medoc(ID)
+            Dim Medicament = BDD.AfficheMedicament(ID)
+            Dim Motif = BDD.AfficheMotifduCompteRendu(ID)
+            Dim Medecin = BDD.AfficheMedecinduCompteRendu(ID)
+
+            'Affichage du nom
+            LabelNom.Text = BDD.afficheNom(login)
+
+            'Affichage du prenom
+            Labelprenom.Text = BDD.affichePrenom(login)
+
+            'Affichage de la hierarchie
+            LabelHierarchie.Text = BDD.afficheHierarchie(login)
+
+            LabelVisiteurSelectionner.Text = ""
+
+            LabelID.Text = ID
+            LabelBilan.Text = Bilan
+            LabelDte.Text = datefinal
+            LabelQTE_Medoc.Text = QTE_Medoc
+            LabelMedicament.Text = Medicament
+            LabelMotif.Text = Motif
+            LabelMedecin.Text = Medecin
+
         Else
+
             Dim login As String = BDD.afficheLogin()
             Dim idVisiteur = BDD.afficheIdVisiteur(login)
             Dim dte = ProfilVisiteurMedical.ComboBoxCompteRendu.Text.Substring(16, 10)
